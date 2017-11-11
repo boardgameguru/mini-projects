cc8532de12946b6de02ff2faa1d1935b4bb2bbe5
Thomas Spencer <t.spencer1990@hotmail.com>
2017-11-10 21:42:34 -0500

    Added functionality to safety_check method.

--- mars-rover/platform.rb
+++ mars-rover/platform.rb
@@ -5,4 +5,17 @@ class Platform
     @max_y = max_y
   end
 
+# We want the plateau to make sure that the rovers can never drive off of it. I'll create a method to check that a movement would not cause the rover to go to far.
+  def safety_check(rover)
+    case rover.direction
+    when "N"
+       return rover.yaxis + 1 <= @max_y
+    when "E"
+       return rover.xaxis + 1 <= @max_x
+    when "S"
+       return rover.yaxis - 1 >= 0
+    when "W"
+       return rover.xaxis - 1 >= 0
+    end
+  end
 end
