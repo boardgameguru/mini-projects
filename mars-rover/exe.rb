98ad1b0134daf51118e576ae1f7163a985917c43
Thomas Spencer <t.spencer1990@hotmail.com>
2017-11-10 19:35:38 -0500

    Added first user request for plateau size.

--- mars-rover/exe.rb
+++ mars-rover/exe.rb
@@ -1,2 +1,13 @@
+require_relative "rover"
+
 #In order to solve this problem, we are going to need a few things. First, we will need to be able to create rovers with various states, so we need to make a class and create one.
-rover1 = Rover.new(0, 0, 'N')
+
+#Our first line of input from the user is supposed to be the dimensions for the plateau.
+
+puts "Welcome to Mars Rover. Please first set up your platform's size by inputting two numbers(Example: '0,0')"
+
+platform_sizes = gets.chomp
+
+#Once we have this data, we can save our plateau info.
+
+platform = platform_sizes.split(',')
